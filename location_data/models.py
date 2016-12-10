9f13e6d676ed361e50e89c568810f9fff9ff9b5a
Mike Barkas <mike@barkas.com>
2016-12-10 11:51:36 -0500

    Issue-32: Start location model

--- location_data/models.py
+++ location_data/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+
+    city = models.CharField(max_length=200)
+    state = models.Charfield(max_length=5)
+    zipcode = models.PositiveIntegerField()
+    population = models.PositiveIntegerField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.city

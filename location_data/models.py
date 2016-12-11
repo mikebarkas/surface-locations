5fad0178fc7ba5832ffea873e29e0a909cd6b226
Mike Barkas <mike@barkas.com>
2016-12-10 21:33:21 -0500

    Issue-32: Add location data app to app list is settings.

--- location_data/models.py
+++ location_data/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Location(models.Model):
 
     city = models.CharField(max_length=200)
-    state = models.Charfield(max_length=5)
+    state = models.CharField(max_length=5)
     zipcode = models.PositiveIntegerField()
     population = models.PositiveIntegerField()
     active = models.BooleanField(default=True)

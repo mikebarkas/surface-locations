d7381634dc90bb747353c154883ecc01d9413ed3
Mike Barkas <mike@barkas.com>
2016-12-10 22:32:20 -0500

    Issue-32: Change zip code field to CharField because of leading zeros.

--- location_data/models.py
+++ location_data/models.py
@@ -5,7 +5,7 @@ class Location(models.Model):
 
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=5)
-    zipcode = models.PositiveIntegerField()
+    zipcode = models.CharField(max_length=15)
     population = models.PositiveIntegerField()
     active = models.BooleanField(default=True)
 

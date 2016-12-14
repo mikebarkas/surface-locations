f6d2ad03e977ec65425b5272a01c08aa251b9b0e
Mike Barkas <mike@barkas.com>
2016-12-13 20:26:08 -0500

    Issue-34: Placeholder links for states.

--- location_data/views.py
+++ location_data/views.py
@@ -8,5 +8,5 @@ def state(request):
     return render(request, 'location/state.html', {'data': data})
 
 def city_list(request, state):
-    city_list = Location.objects.filter(state=state.upper())
-    return render(request, 'location/city_list.html', {'cities': city_list})
+    cities = Location.objects.filter(state=state.upper())
+    return render(request, 'location/city_list.html', {'cities': cities})

e7d29dd07ba722ac5a2a003608dac1deb3278282
Mike Barkas <mike@barkas.com>
2016-12-13 19:36:35 -0500

    Issue-32: Start location urls.

--- location_data/urls.py
+++ location_data/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(),
+    url(r'^$', views.state, name='state'),
+    url(r'^(?P<state>[\w-]+)/$', views.state_list, name='state-list'),
 ]
 

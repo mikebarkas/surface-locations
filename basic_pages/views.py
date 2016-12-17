b89358897e7b8362d32ee18bd3bd9724a82afcba
Mike Barkas <mike@barkas.com>
2016-12-17 16:02:32 -0500

    Issue-45: Add basic page views and url.

--- basic_pages/views.py
+++ basic_pages/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html', {'index': 'index'})
+
+
+def about(request):
+    content = 'The page is a Django project used for learning.'
+    return render(request, 'basic_pages/about.html', {'context': content})

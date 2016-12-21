1355d8acc4bf331de1fc4560511896086816da4f
Mike Barkas <mike@barkas.com>
2016-12-20 20:56:40 -0500

    Issue-61: Limit regex for matching states in the URL.

--- location_data/urls.py
+++ location_data/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.state_select, name='state-select'),
-    url(r'^(?P<state>[\w-]+)/$', views.city_list, name='city-list'),
-    url(r'^(?P<state>[\w-]+)/(?P<city>[\w-]+)/$', views.city_detail, name='city-detail'),
+    url(r'^(?P<state>[\w]{2})/$', views.city_list, name='city-list'),
+    url(r'^(?P<state>[\w]{2})/(?P<city>[\w-]+)/$', views.city_detail, name='city-detail'),
 ]

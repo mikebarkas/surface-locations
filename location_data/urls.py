93463ae1bfd6f40bff1e94adc3eadd9c0fa586f4
Mike Barkas <mike@barkas.com>
2017-01-13 16:38:23 -0500

    Issue-71: Add url for json graph example.

--- location_data/urls.py
+++ location_data/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.StateSelect.as_view(), name='state-select'),
+    url(r'^api-json/(?P<state>[\w]{2})/$', views.api_json, name='api-json'),
     url(r'^(?P<state>[\w]{2})/$', views.city_list, name='city-list'),
     url(r'^(?P<state>[\w]{2})/(?P<city>[\w-]+)/$', views.city_detail, name='city-detail'),
 ]

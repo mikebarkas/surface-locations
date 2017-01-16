92103025396c62d076692a8176be020859ebe9be
Mike Barkas <mike@barkas.com>
2017-01-16 17:53:14 -0500

    Issue-87: Add state list to view.

--- basic_pages/views.py
+++ basic_pages/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from location_data.us_states import us_states
+
 
 def index(request):
     return render(request, 'index.html', {'index': 'index'})
 
 
 def about(request):
-    content = 'The page is a Django project used for learning.'
-    return render(request, 'basic_pages/about.html', {'context': content})
+    context = {
+       'content': 'The page is a Django project used for learning.',
+       'us_states': us_states()
+    }
+    return render(request, 'basic_pages/about.html', context)

b6f7a72f539c4ea07cda641ba9caa41ce04f2a65
Mike Barkas <mike@barkas.com>
2017-03-13 16:04:25 -0400

    Issue-89: Add url for city search API.

--- location_data/urls.py
+++ location_data/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.StateSelect.as_view(), name='state-select'),
     url(r'^api-json/$', views.api_json, name='api-json'),
+    url(r'^api-city-search/$', views.api_city_search, name='api-city-search'),
     url(r'^city-search/$', views.CitySearch.as_view(), name='city-search'),
     url(r'^(?P<state>[\w]{2})/$', views.city_list, name='city-list'),
     url(r'^(?P<state>[\w]{2})/(?P<city>[\w-]+)/$', views.city_detail, name='city-detail'),

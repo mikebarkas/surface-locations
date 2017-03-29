91664180e879e04edff92e01df1cfe51e7a17d12
Mike Barkas <mike@barkas.com>
2017-03-29 13:21:25 -0400

    Issue-104: Change URL for new state page.

--- location_data/urls.py
+++ location_data/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^api-json/$', views.api_json, name='api-json'),
     url(r'^api-city-search/$', views.api_city_search, name='api-city-search'),
     url(r'^city-search/$', views.CitySearch.as_view(), name='city-search'),
-    url(r'^(?P<state>[\w]{2})/$', views.city_list, name='city-list'),
+    url(r'^(?P<state>[\w]{2})/$', views.state_page, name='state-page'),
     url(r'^(?P<state>[\w]{2})/all$', views.city_list, name='city-list'),
     url(r'^(?P<state>[\w]{2})/(?P<city>[\w-]+)/$', views.city_detail, name='city-detail'),
 ]
